f9d5fa56a2bb9bbc7a847c837a5fd722151addb1
Vinicius Alves <103587422+Viniciusalves16@users.noreply.github.com>
2025-10-22 19:04:01 -0300

    Inicializando projeto e incluindo dependencias necessárias

---
+++ src/main/java/com/example/vehicle_data_validator/VehicleDataValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_data_validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleDataValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleDataValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vehicle_data_validator/VehicleDataValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_data_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleDataValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
